618c39072973e2330b97b6f4c659256a0bdf0f52
deda161 <ededemba@proton.me>
2026-02-11 14:20:43 +0300

    fix(parser): incerrect artifact name after convertion

--- data_parser.py
+++ data_parser.py
@@ -118,7 +118,7 @@ class DataParser:
 
                         # Normalize artifact name
                         artifavt_name = str(row['артэфакт']).strip() if pd.notna(row['артэфакт']) else 'none'
-                        artifavt_name = self.__convert_artifact_name(artifavt_name)
+                        self.__convert_artifact_name(artifavt_name)
 
                         find_record = {
                             'name': artifavt_name,
