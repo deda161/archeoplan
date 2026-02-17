5656c8633ee319ef8b0c02163e54de558c154587
deda161 <ededemba@proton.me>
2026-02-17 14:55:30 +0300

    feat: add ability to generate all layers plots

--- graphic_creator.py
+++ graphic_creator.py
@@ -8,31 +8,36 @@ from datetime import datetime
 
 class GraphicCreator:
     def __init__(self):
-        self.fig = None
-        self.ax = None
+        self.figs = []
+        self.axes = []
 
-    def __prepare_background(self, rows, columns):
-        self.fig, self.ax = plt.subplots(figsize=(12, 9)) # in inches
-        self.fig.patch.set_facecolor('#fafafa')
-        plt.subplots_adjust(left=0.2) # for legend
+    def __prepare_background(self, rows, columns, layers_count=1):
 
-        self.ax.set_xlim(-0.5, columns - 0.5)
-        self.ax.set_ylim(-0.5, rows - 0.5)
-        self.ax.set_aspect('equal')
-        self.ax.set_facecolor('#fafafa')
-        self.ax.axis('off')
+        for _ in range(layers_count):
+            fig, ax = plt.subplots(figsize=(12, 9)) # in inches
+            fig.patch.set_facecolor('#fafafa')
+            plt.subplots_adjust(left=0.2) # for legend
 
-        self.ax.set_xticks([])
-        self.ax.set_yticks([])
+            ax.set_xlim(-0.5, columns - 0.5)
+            ax.set_ylim(-0.5, rows - 0.5)
+            ax.set_aspect('equal')
+            ax.set_facecolor('#fafafa')
+            ax.axis('off')
 
-    def __save_plot_as_file(self, file_name):
+            ax.set_xticks([])
+            ax.set_yticks([])
+
+            self.figs.append(fig)
+            self.axes.append(ax)
+
+    def __save_plot_as_file(self, file_name, layer):
         current_datetime = datetime.now()
-        folder_path = f"output_{current_datetime.strftime('%Y_%m_%d__%H_%M_%S')}"
+        folder_path = f"output_{current_datetime.strftime('%Y_%m_%d__%H_%M')}"
         os.makedirs(folder_path, exist_ok=True)
 
-        self.fig.savefig(f'{folder_path}/{file_name}.png', dpi=300, bbox_inches='tight')
+        self.figs[layer].savefig(f'{folder_path}/{file_name}.png', dpi=300, bbox_inches='tight')
 
-    def __draw_grid(self, rows, columns, squares_matrix):
+    def __draw_grid(self, rows, columns, squares_matrix, layer=0):
         for row in range(rows):
             for column in range(columns):
                 if (squares_matrix[row][column] != -1):
@@ -43,20 +48,20 @@ class GraphicCreator:
                                         facecolor='#fafafa',
                                         edgecolor='black',
                                         linewidth=1)
-                    self.ax.add_patch(rect)
+                    self.axes[layer].add_patch(rect)
                     # add square number as text
                     if x_coord == 0:
-                        self.ax.text(x_coord - 0.6, y_coord, f'{squares_matrix[row][column]}',
+                        self.axes[layer].text(x_coord - 0.6, y_coord, f'{squares_matrix[row][column]}',
                                 ha='center', va='center',
                                 fontsize=10, color='black', alpha=0.7)
                     elif y_coord == 0 and x_coord != 0:
-                        self.ax.text(x_coord, y_coord - 0.55, f'{squares_matrix[row][column]}',
+                        self.axes[layer].text(x_coord, y_coord - 0.55, f'{squares_matrix[row][column]}',
                                 ha='center', va='center',
                                 fontsize=10, color='black', alpha=0.7)
                     elif y_coord != rows - 1 and x_coord != 0:
                         continue
                     elif y_coord == rows - 1 and x_coord != 0:
-                        self.ax.text(x_coord, y_coord + 0.55, f'{squares_matrix[row][column]}',
+                        self.axes[layer].text(x_coord, y_coord + 0.55, f'{squares_matrix[row][column]}',
                                 ha='center', va='center',
                                 fontsize=10, color='black', alpha=0.7)
 
@@ -72,50 +77,56 @@ class GraphicCreator:
         #             solid_capstyle='butt')
 
 
-    def __prepare_artifacts_data(self, artifacts_raw_data):
+    def __prepare_layers_data(self, artifacts_raw_data) -> tuple[list, int]:
         self.artifacts = []
-        if artifacts_raw_data[0]['layer'] == '80-90':
-            self.artifacts = artifacts_raw_data[0]['squares']
-        else:
-            self.artifacts = artifacts_raw_data[1]['squares']
+        layers_array = []
+
+        for layer in artifacts_raw_data:
+            layers_array.append(layer['layer'])
+
+        return layers_array, len(layers_array)
 
     def make_plots(self, rows, columns, squares_matrix, artifacts_raw_data):
-        self.__prepare_background(rows, columns)
-        self.__draw_grid(rows, columns, squares_matrix)
-        self.__prepare_artifacts_data(artifacts_raw_data)
+        layers_array, layers_count = self.__prepare_layers_data(artifacts_raw_data)
+        self.__prepare_background(rows, columns, layers_count)
 
-        artifacts_manager = ArtifactsManager()
+        for layer in range(layers_count):
+            self.__draw_grid(rows, columns, squares_matrix, layer)
 
-        for row in range(rows):
-            for column in range(columns):
-                current_square = squares_matrix[row][column]
-                if current_square != -1:
-                    artifacts_square = next((d for d in self.artifacts if d['square'] == current_square), None)
-                    x_min = column - 0.5
-                    y_min = rows - row - 1 - 0.5
-                    if artifacts_square != None and artifacts_square['finds'] != 'no':
-                        counter = 0
-                        for artifact in artifacts_square['finds']:
+            self.artifacts = artifacts_raw_data[layer]['squares']
 
-                            if (artifact['xy'][0] < 0 or artifact['xy'][1] < 0) or (artifact['xy'][0] > 100 or artifact['xy'][1] > 100) or math.isnan(artifact['xy'][0]) or math.isnan(artifact['xy'][1]) or math.isinf(artifact['xy'][0]) or math.isinf(artifact['xy'][1]):
-                                print(f"Incorrect coords: {artifact['xy']} for artifact {artifact['name']} in square {current_square}")
+            artifacts_manager = ArtifactsManager()
+
+            for row in range(rows):
+                for column in range(columns):
+                    current_square = squares_matrix[row][column]
+                    if current_square != -1:
+                        artifacts_square = next((d for d in self.artifacts if d['square'] == current_square), None)
+                        x_min = column - 0.5
+                        y_min = rows - row - 1 - 0.5
+                        if artifacts_square != None and artifacts_square['finds'] != 'no':
+                            counter = 0
+                            for artifact in artifacts_square['finds']:
+
+                                if (artifact['xy'][0] < 0 or artifact['xy'][1] < 0) or (artifact['xy'][0] > 100 or artifact['xy'][1] > 100) or math.isnan(artifact['xy'][0]) or math.isnan(artifact['xy'][1]) or math.isinf(artifact['xy'][0]) or math.isinf(artifact['xy'][1]):
+                                    print(f"Incorrect coords: {artifact['xy']} for artifact {artifact['name']} in square {current_square}")
+                                    counter += 1
+                                    continue
+
                                 counter += 1
-                                continue
+                                x = (artifact['xy'][0] / 100) + x_min
+                                y = (artifact['xy'][1] / 100) + y_min
+                                artifact_symble = artifacts_manager.get_offset_image_of_artifact(artifact['name'])
 
-                            counter += 1
-                            x = (artifact['xy'][0] / 100) + x_min
-                            y = (artifact['xy'][1] / 100) + y_min
-                            artifact_symble = artifacts_manager.get_offset_image_of_artifact(artifact['name'])
+                                if artifact_symble != None:
+                                    ab = AnnotationBbox(artifact_symble, (x, y), frameon=False)
+                                    self.axes[layer].add_artist(ab)
 
-                            if artifact_symble != None:
-                                ab = AnnotationBbox(artifact_symble, (x, y), frameon=False)
-                                self.ax.add_artist(ab)
+            # Plot name
+            self.axes[layer].set_title(f'Пласт {layers_array[layer]}', fontsize=14, fontweight='bold', pad=20)
 
-        # Plot name
-        self.ax.set_title(f'Пласт {artifacts_raw_data[0]['layer']}', fontsize=14, fontweight='bold', pad=20)
+            plt.tight_layout()
 
-        plt.tight_layout()
-
-        self.__save_plot_as_file('graph')
+            self.__save_plot_as_file(f'graph_{layers_array[layer]}', layer)
 
         plt.show()
